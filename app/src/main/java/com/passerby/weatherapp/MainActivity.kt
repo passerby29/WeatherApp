c839ef57e1a2f00806aa9b4335a40c887f3fcde1
passerby29 <78204469+passerby29@users.noreply.github.com>
2022-07-24 11:43:10 +0300

    version 1.0.2

--- app/src/main/java/com/passerby/weatherapp/MainActivity.kt
+++ app/src/main/java/com/passerby/weatherapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.passerby.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        city_textview.text = "Simferopol"
+        date_textview.text = "24 july"
+        weather_image_add.setImageResource(R.drawable.ic_sun_icon)
+        weather_state_tv.text = "sunny"
+        weather_image_main.setImageResource(R.mipmap.sun_main1x)
     }
 }
