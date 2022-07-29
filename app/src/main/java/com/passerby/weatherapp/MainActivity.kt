0b1e3cb788be3dddb8b3c61bd5d36c82a2e3ce9d
passerby29 <78204469+passerby29@users.noreply.github.com>
2022-07-29 20:11:53 +0300

    version 1.0.5

--- app/src/main/java/com/passerby/weatherapp/MainActivity.kt
+++ app/src/main/java/com/passerby/weatherapp/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.passerby.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.location.*
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.passerby.weatherapp.view.adapters.MainDailyListAdapter
 import com.passerby.weatherapp.view.adapters.MainHourlyListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
+const val GEO_LOCATION_REQUEST_CODE_SUCCESS = 1
+const val TAG = "GEO_TEST"
+
 class MainActivity : AppCompatActivity() {
+
+    private val geoService by lazy { LocationServices.getFusedLocationProviderClient(this) }
+    private val locationRequest by lazy { initLocationRequest() }
+    private lateinit var mLocation: Location
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermission()
         initView()
 
         main_hourly_list.apply {
@@ -24,6 +40,8 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
             setHasFixedSize(true)
         }
+
+        geoService.requestLocationUpdates(locationRequest, geoCallBack, null)
     }
 
     private fun initView() {
@@ -33,4 +51,66 @@ class MainActivity : AppCompatActivity() {
         weather_state_tv.text = "sunny"
         weather_image_main.setImageResource(R.mipmap.sun_main1x)
     }
+
+    //-----------------location code-----------------
+
+    private fun initLocationRequest(): LocationRequest {
+        val request = LocationRequest.create()
+        return request.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    private val geoCallBack = object : LocationCallback() {
+        override fun onLocationResult(geoRes: LocationResult) {
+            Log.d(TAG, "onLocationResult: ${geoRes.locations.size}")
+            for (location in geoRes.locations) {
+                mLocation = location
+                Log.d(TAG, "onLocationResult: lat: ${location.latitude}; ${location.longitude}")
+            }
+        }
+    }
+
+    //------initial activity code
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d(TAG, "onRequestPermissionsResult: $requestCode")
+    }
+
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("We need geo data")
+                .setMessage("Allow user your geo data, pwease")
+                .setPositiveButton("OK") { _, _ ->
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        GEO_LOCATION_REQUEST_CODE_SUCCESS
+                    )
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                        GEO_LOCATION_REQUEST_CODE_SUCCESS
+                    )
+                }
+                .setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        }
+    }
+
+    //-----------------location code-----------------
 }
